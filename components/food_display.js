2a8035364722ad844425db162cf6e67aa5c74f44
Prakita Renatin <ge25zow@mytum.de>
2021-10-15 20:21:58 +0200

    Updated food_displa

--- components/food_display.js
+++ components/food_display.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import GlobalContext from '../utils/global-context'
+import Link from 'next/link'
 
 export default function FoodDisplay() {
     const global = useContext(GlobalContext);
@@ -22,47 +23,93 @@ export default function FoodDisplay() {
     function computeColor(food_item) {
         let currentDate = new Date();
         let food_expir_date = new Date(food_item.expir_date)
-        if(currentDate - food_expir_date < 0) {
+        if(food_expir_date - currentDate < 0) {
             return "#dd5823"; //Red
-        }else if((currentDate - food_expir_date) < (7 * 24 * 60 * 60 * 1000)){
+        }else if((food_expir_date - currentDate) < (7 * 24 * 60 * 60 * 1000)){
             return "#d1bc23"; //Yellow
         }else {
             return "#6fc41f"; //Green
         }
     }
-    //const foodList = [
-    //    {
-    //        id: 123,
-    //        name: "Paprika",
-    //        quantity: 3,
-    //        unit: "pcs",
-    //        expir_date: new Date(2022, 11, 21)
-    //    },
-    //    {
-    //        id: 12,
-    //        name: "Tomatoes",
-    //        quantity: 5,
-    //        unit: "pcs",
-    //        expir_date: new Date(2021, 9, 12)
-    //    },
-    //    {
-    //        id: 111,
-    //        name: "Bananas",
-    //        quantity: 2,
-    //        unit: "pcs",
-    //        expir_date: new Date(2018, 11, 24)
-    //    },
-    //];
-    const foodList = global.foodList;
+    const foodList = [
+        {
+            id: 123,
+            name: "Paprika",
+            quantity: 3,
+            unit: "pcs",
+            expir_date: new Date(2022, 11, 21).toString()
+        },
+        {
+            id: 12,
+            name: "Tomatoes",
+            quantity: 5,
+            unit: "pcs",
+            expir_date: new Date(2021, 9, 12).toString()
+        },
+        {
+            id: 111,
+            name: "Bananas",
+            quantity: 2,
+            unit: "pcs",
+            expir_date: new Date(2018, 11, 24).toString()
+        },
+        {
+            id: 111,
+            name: "Bananas",
+            quantity: 2,
+            unit: "pcs",
+            expir_date: new Date(2021, 9, 24).toString()
+        },
+        {
+            id: 111,
+            name: "Bananas",
+            quantity: 2,
+            unit: "pcs",
+            expir_date: new Date(2022, 11, 24).toString()
+        },
+        {
+            id: 111,
+            name: "Bananas",
+            quantity: 2,
+            unit: "pcs",
+            expir_date: new Date(2022, 11, 24).toString()
+        },
+        {
+            id: 111,
+            name: "Bananas",
+            quantity: 2,
+            unit: "pcs",
+            expir_date: new Date(2022, 11, 24).toString()
+        },
+        {
+            id: 111,
+            name: "Bananas",
+            quantity: 2,
+            unit: "pcs",
+            expir_date: new Date(2022, 11, 24).toString()
+        },
+        {
+            id: 111,
+            name: "Bananas",
+            quantity: 2,
+            unit: "pcs",
+            expir_date: new Date(2022, 11, 24).toString()
+        },
+        {
+            id: 111,
+            name: "Bananas",
+            quantity: 2,
+            unit: "pcs",
+            expir_date: new Date(2022, 11, 24).toString()
+        },
+    ];
+    //const foodList = global.foodList;
     foodList.sort(function (f1, f2) {
-        return (new Date(f2.expir_date)) - (new Date(f1.expir_date));
+        return (new Date(f1.expir_date)) - (new Date(f2.expir_date));
     })
-    let cnter = 0;
-    cnter += 1;
     return (
         <>
-            <p> Food Display </p>
-            <>
+            <div className="food_display_frame">
               {
                 foodList.map((item, index) =>
                   <p 
@@ -73,7 +120,7 @@ export default function FoodDisplay() {
                   </p>
                 )
               }
-            </>
+            </div>
         </>
     );
 }
