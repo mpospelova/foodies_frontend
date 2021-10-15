63be436c5ed460342fdf388a50caf920b1272bd3
Prakita Renatin <ge25zow@mytum.de>
2021-10-15 16:37:32 +0200

    Added empty landing_page

---
+++ components/landing_page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LandingPage() {
+  return (
+    <div className="landing_page">
+      <h2 className="landing_page_title">This is the landing page</h2>
+    </div>
+  );
+}
