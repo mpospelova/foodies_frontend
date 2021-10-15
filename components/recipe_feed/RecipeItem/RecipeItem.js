bd9a1960df2e430fd7829c60627c179c9f59fc2e
sutedalm <37000551+sutedalm@users.noreply.github.com>
2021-10-15 20:32:15 +0200

    make recipe from list

--- components/recipe_feed/RecipeItem/RecipeItem.js
+++ components/recipe_feed/RecipeItem/RecipeItem.js
@@ -2,15 +2,11 @@ import React from "react";
 import { SwiperSlide } from "swiper/react";
 import styles from "./RecipeItem.module.css";
 
-const RecipeItem = () => {
+const RecipeItem = ({ name, summary }) => {
   return (
     <div className={styles.RecipeItem__container}>
-      <h1>Chicken Curry</h1>
-      <div className={styles.RecipeItem__summary}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
-        excepturi quisquam labore pariatur voluptatibus? Delectus laborum nam ex
-        quia soluta!
-      </div>
+      <h1>{name}</h1>
+      <div className={styles.RecipeItem__summary}>{summary}</div>
     </div>
   );
 };
--- components/recipe_feed/RecipeList/RecipeList.js
+++ components/recipe_feed/RecipeList/RecipeList.js
@@ -5,18 +5,44 @@ import styles from "./RecipeList.module.css";
 
 import RecipeItem from "../RecipeItem/RecipeItem";
 const RecipeList = () => {
+  const recipes = [
+    {
+      id: "0",
+      name: "Curry",
+      summary:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit soluta quaerat modi porro aperiam facilis, maxime quas tempore fugiat accusantium?",
+      ingredients: [],
+    },
+    {
+      id: "1",
+      name: "Rendang",
+      summary:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit soluta quaerat modi porro aperiam facilis, maxime quas tempore fugiat accusantium?",
+      ingredients: [],
+    },
+    {
+      id: "2",
+      name: "Rendang",
+      summary:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit soluta quaerat modi porro aperiam facilis, maxime quas tempore fugiat accusantium?",
+      ingredients: [],
+    },
+    {
+      id: "3",
+      name: "Rendang",
+      summary:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit soluta quaerat modi porro aperiam facilis, maxime quas tempore fugiat accusantium?",
+      ingredients: [],
+    },
+  ];
   return (
     <div className={styles.RecipeList__container}>
       <Swiper slidesPerView={"auto"} spaceBetween={5}>
-        <SwiperSlide className={styles.swiperSlide}>
-          <RecipeItem />
-        </SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>
-          <RecipeItem />
-        </SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>
-          <RecipeItem />
-        </SwiperSlide>
+        {recipes.map(({ id, name, summary }) => (
+          <SwiperSlide key={id} className={styles.swiperSlide}>
+            <RecipeItem name={name} summary={summary} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
