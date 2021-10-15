527d258bcbf446058305ee56505bce8520c7d770
Prakita Renatin <ge25zow@mytum.de>
2021-10-15 17:49:04 +0200

    Added dynamic colors for food_display (food list still hardcoded)

--- components/food_display.js
+++ components/food_display.js
@@ -1,31 +1,52 @@
 export default function FoodDisplay() {
+    function computeColor(food_item) {
+        let currentDate = new Date();
+        if(currentDate - food_item.expir_date < 0) {
+            return "#dd5823"; //Red
+        }else if((currentDate - food_item.expir_date) < (7 * 24 * 60 * 60 * 1000)){
+            return "#d1bc23"; //Yellow
+        }else {
+            return "#6fc41f"; //Green
+        }
+    }
     const foodList = [
         {
             id: 123,
             name: "Paprika",
             quantity: 3,
-            unit: "pcs"
+            unit: "pcs",
+            expir_date: new Date(2022, 11, 21)
         },
         {
             id: 12,
             name: "Tomatoes",
             quantity: 5,
-            unit: "pcs"
+            unit: "pcs",
+            expir_date: new Date(2021, 9, 12)
         },
         {
             id: 111,
             name: "Bananas",
             quantity: 2,
-            unit: "pcs"
+            unit: "pcs",
+            expir_date: new Date(2018, 11, 24)
         },
     ];
+    foodList.sort(function (f1, f2) {
+        return f2.expir_date - f1.expir_date;
+    })
     return (
         <>
             <p> Test test test </p>
             <>
               {
                 foodList.map((item, index) =>
-                  <p className="food_display_item" key={index}>{item.name} {item.quantity} {item.unit}</p>
+                  <p 
+                    className="food_display_item"
+                    key={index}
+                    style={{background:computeColor(item)}}>
+                    {item.name} {item.quantity} {item.unit} {item.expir_date.toString()}
+                  </p>
                 )
               }
             </>
